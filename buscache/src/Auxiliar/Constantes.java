434faaf6ce78938c3a107227d8269a5b26576219
josego85 <josego85@14fcc192-e73f-053c-28c2-a1c09c53e1a7>
2012-10-04 01:18:13 +0000

    Se creo la ventana principal(GUI)

---
+++ buscache/src/Auxiliar/Constantes.java
@@ -0,0 +1,31 @@
+/**
+ *@(#)Constantes.java
+ *
+ * @author proyectosbeta
+ * @version 1.00
+ * @since 1.7
+ */
+/**
+ * Clase que aparecen todas las constantes del Programa Buscador.
+ * @since 1.7
+ */
+package Auxiliar;
+public class Constantes {
+    ////////////////////////////////////////////////////////////////////////////
+    // Todos los titulos.
+    ////////////////////////////////////////////////////////////////////////////
+    /**
+     * Titulo del Programa.
+     * @since 1.7
+     */
+    public static final String TITULO_PROGRAMA = "Buscador v1.0";
+    
+    ////////////////////////////////////////////////////////////////////////////
+    // Varios.
+    ////////////////////////////////////////////////////////////////////////////
+    /**
+     * Incremento de la cantidad de espacio para el titulo.
+     * @since 1.7
+     */
+    public static final String INCREMENTO_CANTIDAD_DE_ESPACIO_TITULO = "     ";
+} // Fin de la clase Constantes.
